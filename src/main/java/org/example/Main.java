9469275dff8d77f747a5c925c03f0cd991e07cd9
kh12v <154763602+kh12v@users.noreply.github.com>
2025-04-19 19:11:10 -0400

    Setting examples

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -226,8 +226,26 @@ class DepositoM {
 
 public class Main {
     public static void main(String[] args) {
-        Expendedor exp = new Expendedor(3, 100);
+        Expendedor exp = new Expendedor(2, 100);
+
+        // Comprar con vuelto
         Comprador c = new Comprador(new Moneda500(), Expendedor.COCA, exp);
+        System.out.println("Bebida: " + c.queBebiste() + "; Vuelto: " + c.cuantoVuelto());
+
+        // Comprar sin vuelto
+        c = new Comprador(new Moneda100(), Expendedor.COCA, exp);
+        System.out.println("Bebida: " + c.queBebiste() + "; Vuelto: " + c.cuantoVuelto());
+
+        // Comprar sin bebidas
+        c = new Comprador(new Moneda1000(), Expendedor.COCA, exp);
+        System.out.println("Bebida: " + c.queBebiste() + "; Vuelto: " + c.cuantoVuelto());
+
+        // Comprar sin moneda
+        c = new Comprador(null, Expendedor.SPRITE, exp);
+        System.out.println("Bebida: " + c.queBebiste() + "; Vuelto: " + c.cuantoVuelto());
+
+        // Comprar bebida no existente
+        c = new Comprador(new Moneda1000(), 3, exp);
         System.out.println("Bebida: " + c.queBebiste() + "; Vuelto: " + c.cuantoVuelto());
     }
 }
